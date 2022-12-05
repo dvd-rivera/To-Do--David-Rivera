1872b7eba4077cf5c98c2bb1894f68dcfeb4cbec
dvd-rivera <51865488+dvd-rivera@users.noreply.github.com>
2022-12-05 14:45:52 -0300

    subida del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Todo } from './model/todo.interfase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'proyecto';
+	title = 'proyecto';
+
+	tasks: Array<Todo> = [];
+
+	displayedColumns: string[] = ['select', 'ID', 'Tarea', 'remove'];
+	dataSource = new MatTableDataSource<Todo>(this.tasks);
+	selection = new SelectionModel<Todo>(true, []);
+
+	newTaskForm = this.fb.group({
+		newTask: new FormControl(''),
+	});
+
+	tasksID = 0;
+
+	isAllSelected() {
+		const numSelected = this.selection.selected.length;
+		const numRows = this.dataSource.data.length;
+		return numSelected === numRows;
+	}
+
+	toggleAllRows() {
+		if (this.isAllSelected()) {
+			this.selection.clear();
+			return;
+		}
+
+		this.selection.select(...this.dataSource.data);
+	}
+
+	checkboxLabel(row?: Todo): string {
+		if (!row) {
+			return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+		}
+		return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+			row.id + 1
+		}`;
+	}
+
+	addTask() {
+		let task = this.newTaskForm.controls.newTask.value;
+		if (task != null && task != undefined) {
+			this.tasks.push({
+				id: this.tasksID + 1,
+				task: task,
+				endDate: new Date(),
+			});
+			this.tasksID += 1;
+		}
+		this.dataSource.data = this.tasks;
+	}
+
+	removeTask(index: number) {
+		let newTaskList = this.tasks.filter((task) => task.id != index);
+		this.tasks = newTaskList;
+		this.dataSource.data = newTaskList;
+	}
+
+	constructor(private fb: FormBuilder) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { TodoComponent } from './todo/todo.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, TodoComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NoopAnimationsModule,
+		ReactiveFormsModule,
+		FormsModule,
+		MatCheckboxModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatTableModule,
+	],
+	exports: [MatCheckboxModule, MatInputModule, MatFormFieldModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
